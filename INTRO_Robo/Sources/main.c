8f7b24a879cb10f68cab380bf7d90325deadb9df
LivioBrunner4 <livio.brunner@stud.hslu.ch>
2017-03-01 09:23:09 +0100

    LED added + Debug probleme

--- INTRO_Robo/Sources/main.c
+++ INTRO_Robo/Sources/main.c
@@ -30,6 +30,8 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "LEDD4.h"
+#include "BitIoLdd1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -49,7 +51,9 @@ int main(void)
 
   /* Write your code here */
   /* For example: for(;;) { } */
-
+  for(;;) {
+  LEDD4_PutVal (TRUE);
+  }
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
   #ifdef PEX_RTOS_START
