f32e3d5975ce923e6e15dce8d1c690cd74206c3c
LivioBrunner4 <livio.brunner@stud.hslu.ch>
2017-04-04 10:47:30 +0200

    LAB 17: Trigger

--- Team_Template/TEAM_Common/Application.c
+++ Team_Template/TEAM_Common/Application.c
@@ -10,7 +10,6 @@
 #include "Application.h"
 #include "Event.h"
 #include "LED.h"
-//#include "LED1.h"  was ist mit dem???
 #include "Trigger.h"   //added by livio
 #include "WAIT1.h"
 #include "CS1.h"
@@ -48,10 +47,10 @@
 #if PL_LOCAL_CONFIG_BOARD_IS_ROBO
 #if PL_CONFIG_HAS_EVENTS
 
-
-static void CallBackLED(void) {			// modified Livio
-    LED1_Neg();										// Where is the best place to call the CallBack function?
+static void CallBackLED(void*p) {			// modified Livio
+    LED1_Neg();
 }
+
 
 
 /* Eventhandler ROBO */
@@ -59,8 +58,7 @@ void APP_EventHandler(EVNT_Handle event) {
 	  /*! \todo handle events */
 	  switch(event) {
 	  case EVNT_STARTUP:
-		  (void)BUZ_PlayTune(BUZ_TUNE_WELCOME);
-		  //(void)TRG_SetTrigger(TRG_LED_BLINK, 0, CallBackLED(), NULL);
+		  //(void)BUZ_PlayTune(BUZ_TUNE_WELCOME);
 	    break;
 	  case EVNT_LED_OFF:
 	    break;
@@ -71,8 +69,8 @@ void APP_EventHandler(EVNT_Handle event) {
 		  LEDPin1_NegVal();
 		break;
 	  case EVNT_SW1_SHORT_PRESSED:
-		  (void)BUZ_PlayTune(BUZ_TUNE_BUTTON);
-		  LEDPin1_NegVal();
+		  //(void)BUZ_PlayTune(BUZ_TUNE_BUTTON);
+		  (void)TRG_SetTrigger(TRG_LED_BLINK, 1000/TRG_TICKS_MS, CallBackLED, NULL);
 		break;
 	  case EVNT_SW1_LONG_PRESSED:
 		  //LEDPin1_ClrVal();     OLD
